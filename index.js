6e9ad40c9abaa3bd77e7dd93007b65d0dbddb571
olyop <oliver.plummer@outlook.com>
2021-06-03 11:47:04 +1000

    Update

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ module.exports = {
 	rules: {
 		"indentation": "tab",
 		"selector-max-id": 1,
+		"property-case": "lower",
 		"string-quotes": "double",
 		"max-nesting-depth": null,
 		"no-eol-whitespace": true,
+		"at-rule-name-case": "lower",
+		"selector-type-case": "lower",
 		"selector-class-pattern": null,
 		"rule-empty-line-before": null,
 		"value-no-vendor-prefix": null,
+		"media-feature-name-case": "lower",
+		"selector-pseudo-class-case": "lower",
 		"no-missing-end-of-source-newline": null,
 		"order/properties-alphabetical-order": null,
+		"block-closing-brace-space-before": "always",
+		"selector-class-pattern": "^([A-Z]?[a-z]*)(-[a-z]+)*$",
 		"scss/dollar-variable-colon-space-after": "always-single-line"
 	}
 }
