353bad1bdcbf2eca175fcf955b87076667ffaf8e
olyop <oliver.plummer@outlook.com>
2022-02-10 00:49:06 +1100

    Update

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-module.exports = {
-	"plugins": [
-		"stylelint-declaration-block-no-ignored-properties",
-		"stylelint-scss",
-	],
-	"overrides": [
-    {
-      "files": ["**/*.scss"],
-      "customSyntax": "postcss-scss"
-    }
-  ],
-	"extends": "stylelint-config-sass-guidelines",
-	"rules": {
-		"indentation": "tab",
-		"selector-max-id": 1,
-		"property-case": "lower",
-		"string-quotes": "double",
-		"max-nesting-depth": null,
-		"no-eol-whitespace": true,
-		"at-rule-name-case": "lower",
-		"selector-type-case": "lower",
-		"rule-empty-line-before": null,
-		"value-no-vendor-prefix": null,
-		"selector-class-pattern": null,
-		"media-feature-name-case": "lower",
-		"selector-pseudo-class-case": "lower",
-		"no-missing-end-of-source-newline": null,
-		"order/properties-alphabetical-order": null,
-		"block-closing-brace-space-before": "always",
-		"scss/dollar-variable-colon-space-after": "always-single-line"
-	}
-}
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    plugins: [
+        "stylelint-declaration-block-no-ignored-properties",
+        "stylelint-scss",
+    ],
+    overrides: [{
+            files: ["**/*.scss"],
+            customSyntax: "postcss-scss"
+        }],
+    extends: "stylelint-config-sass-guidelines",
+    rules: {
+        "indentation": "tab",
+        "selector-max-id": 1,
+        "property-case": "lower",
+        "string-quotes": "double",
+        "max-nesting-depth": null,
+        "no-eol-whitespace": true,
+        "at-rule-name-case": "lower",
+        "selector-type-case": "lower",
+        "rule-empty-line-before": null,
+        "value-no-vendor-prefix": null,
+        "selector-class-pattern": null,
+        "media-feature-name-case": "lower",
+        "selector-pseudo-class-case": "lower",
+        "no-missing-end-of-source-newline": null,
+        "order/properties-alphabetical-order": null,
+        "block-closing-brace-space-before": "always",
+        "function-parentheses-space-inside": "never-single-line",
+        "scss/dollar-variable-colon-space-after": "always-single-line",
+    },
+};
+exports.default = config;
---
+++ index.ts
@@ -0,0 +1,35 @@
+import { Config } from "stylelint"
+
+const config: Config = {
+	plugins: [
+		"stylelint-declaration-block-no-ignored-properties",
+		"stylelint-scss",
+	],
+	overrides: [{
+		files: ["**/*.scss"],
+		customSyntax: "postcss-scss"
+	}],
+	extends: "stylelint-config-sass-guidelines",
+	rules: {
+		"indentation": "tab",
+		"selector-max-id": 1,
+		"property-case": "lower",
+		"string-quotes": "double",
+		"max-nesting-depth": null,
+		"no-eol-whitespace": true,
+		"at-rule-name-case": "lower",
+		"selector-type-case": "lower",
+		"rule-empty-line-before": null,
+		"value-no-vendor-prefix": null,
+		"selector-class-pattern": null,
+		"media-feature-name-case": "lower",
+		"selector-pseudo-class-case": "lower",
+		"no-missing-end-of-source-newline": null,
+		"order/properties-alphabetical-order": null,
+		"block-closing-brace-space-before": "always",
+		"function-parentheses-space-inside": "never-single-line",
+		"scss/dollar-variable-colon-space-after": "always-single-line",
+	},
+}
+
+export default config
